049b318787a23dfaf0ad16ac92fc50e747d88447
filip <filip@10.87.240.137>
2016-11-11 18:16:20 +0100

    Modified the path of images.

--- MechanicalParts/src/tests/ConvolutionTest.java
+++ MechanicalParts/src/tests/ConvolutionTest.java
@@ -7,16 +7,21 @@ import model.PgmUtilities;
 
 public class ConvolutionTest {
     
-public static final String IMG_PATH = "./images/1.-images-_1_.pgm";
+public static final String IN_PATH = "./images/1.-images-_1_.pgm";
 public static final String OUTPUT_PATH = "./images/1.-images-_1_OUT.pgm";
     
     public static void main(String[] args) {
         
     PgmUtilities pgmUtilities = new PgmUtilities();
-    PGM inputPgm = pgmUtilities.readPGM(IMG_PATH);
+    
+    PGM inputPgm = pgmUtilities.readPGM(IN_PATH);
+    
     Filter filter = new Filter(new int[]{1,2,1,0,0,0,-1,-2,-1});
+    
     PGM outputPgm = pgmUtilities.filterConvolution(inputPgm, filter);
-    pgmUtilities.writePGM(outputPgm, IMG_PATH);
+    
+    pgmUtilities.writePGM(outputPgm, OUTPUT_PATH);
+    
     }
     
 }
