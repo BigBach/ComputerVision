fd0bcf2e18b72c437d616f253194d1de446780d6
Markenos <marco.robutti01@universitadipavia.it>
2016-11-12 17:48:13 +0100

    Resolved some bug with the convolutionFilter method in PgmUtilies class and added the isotropicFilter method in the same class.

--- MechanicalParts/src/tests/ConvolutionTest.java
+++ MechanicalParts/src/tests/ConvolutionTest.java
@@ -9,14 +9,16 @@ public class ConvolutionTest {
     
 public static final String IMG_PATH = "./images/1.-images-_1_.pgm";
 public static final String OUTPUT_PATH = "./images/1.-images-_1_OUT.pgm";
+public static final String MODULE_ISOTROPIC_PATH = "./images/1.-images-_1_MODULE_ISOTROPIC.pgm";
     
     public static void main(String[] args) {
         
     PgmUtilities pgmUtilities = new PgmUtilities();
     PGM inputPgm = pgmUtilities.readPGM(IMG_PATH);
-    Filter filter = new Filter(new int[]{1,2,1,0,0,0,-1,-2,-1});
+    Filter filter = new Filter(new double[]{0,1,0,1,-5,1,0,1,0});
     PGM outputPgm = pgmUtilities.filterConvolution(inputPgm, filter);
-    pgmUtilities.writePGM(outputPgm, IMG_PATH);
+    pgmUtilities.writePGM(outputPgm, OUTPUT_PATH);
+    pgmUtilities.writePGM(pgmUtilities.isotropicFilter(inputPgm)[0], MODULE_ISOTROPIC_PATH);
     }
     
 }
